2097f066ca852a02a737a564b5c2265f6f728c41
Patrick Wang <patrick@covar.com>
2020-07-21 20:38:50 -0400

    Add long description for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 """Setup file for reasoner-validator package."""
 from setuptools import setup
 
+with open('README.md', 'r') as stream:
+    long_description = stream.read()
+
 setup(
     name='reasoner-validator',
     version='0.9.2.1.0.0',
@@ -8,6 +11,8 @@ setup(
     author_email='patrick@covar.com',
     url='https://github.com/NCATSTranslator/reasoner-validator',
     description='Validation tools for Reasoner API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['reasoner_validator', 'reasoner_validator.data'],
     package_data={'reasoner_validator.data': ['*.yaml']},
     include_package_data=True,
