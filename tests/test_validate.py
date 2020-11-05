fea73aa4418df4ab22bb0d9be7f674f9b97c0b11
Patrick Wang <patrick@covar.com>
2020-11-05 09:18:28 -0500

    Update tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -3,25 +3,27 @@ import pytest
 
 from jsonschema.exceptions import ValidationError
 
-from reasoner_validator import validate_Query, validate_Credentials
+from reasoner_validator import validate_EdgeBinding, validate_Query
 
 
-def test_validate():
-    """Test validate()."""
+def test_query():
+    """Test validate_Query()."""
     validate_Query({
-        'message': {}
+        'message': {},
     })
     with pytest.raises(ValidationError):
         validate_Query({
             'foo': {},
             'bar': {},
         })
-    validate_Credentials({
-        'username': 'foo',
-        'password': 'bar',
+
+
+def test_edgebinding():
+    """Test validate_EdgeBinding()."""
+    validate_EdgeBinding({
+        'id': 'hello',
     })
     with pytest.raises(ValidationError):
-        validate_Credentials({
-            'username': 1,
-            'password': 'bar',
+        validate_EdgeBinding({
+            'foo': {},
         })
