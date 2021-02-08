97edeb7012ba264324ef28af64ff8c53b815ed24
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-08 09:37:57 -0500

    Fix nullable -> type: null conversion

--- reasoner_validator/util.py
+++ reasoner_validator/util.py
@@ -3,20 +3,19 @@
 
 def fix_nullable(schema) -> None:
     """Fix nullable schema."""
-    if "$ref" in schema or "type" in schema:
-        schema = {
-            "oneOf": [
-                schema,
-                {"type": "null"},
-            ]
-        }
-        return
     if "oneOf" in schema:
         schema["oneOf"].append({"type": "null"})
         return
     if "anyOf" in schema:
         schema["anyOf"].append({"type": "null"})
         return
+    schema["oneOf"] = [
+        {
+            key: schema.pop(key)
+            for key in list(schema.keys())
+        },
+        {"type": "null"},
+    ]
 
 
 def openapi_to_jsonschema(schema) -> None:
