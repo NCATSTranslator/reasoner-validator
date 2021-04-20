e3afa133b92f5806b98209a53dffb772bdad218e
Chase Yakaboski <chase.a.yakaboski@dartmouth.edu>
2021-04-19 20:51:28 -0400

    Added support for TRAPI v1.0 and v1.1 validation.

--- reasoner_validator/__init__.py
+++ reasoner_validator/__init__.py
@@ -20,54 +20,62 @@ from . import data  # relative-import the *package* containing the templates
 
 from .util import openapi_to_jsonschema
 
-to_load = pkg_resources.read_text(data, 'TranslatorReasonerAPI.yml')
+TRAPI_VERSIONS = ['1.0']
+to_loads = {}
+specs = {}
+components = {}
+for _version in TRAPI_VERSIONS:
+    to_loads[_version] = pkg_resources.read_text(data, 'TranslatorReasonerAPI-{}.yml'.format(_version))
+    specs[_version] = yaml.load(to_loads[_version], Loader=Loader)
+    components[_version] = specs[_version]['components']['schemas']
 
-spec = yaml.load(to_load, Loader=Loader)
-components = spec['components']['schemas']
-for component_name, schema in components.items():
-    openapi_to_jsonschema(schema)
-for component_name in components:
-    # build json schema against which we validate
-    other_components = copy.deepcopy(components)
-    schema = other_components.pop(component_name)
-    schema['components'] = {'schemas': other_components}
+    for component_name, schema in components[_version].items():
+        openapi_to_jsonschema(schema)
+    for component_name in components[_version]:
+        # build json schema against which we validate
+        other_components = copy.deepcopy(components[_version])
+        schema = other_components.pop(component_name)
+        schema['components'] = {'schemas': other_components}
 
-    def validate(obj, schema=schema):
-        """Validate object against schema."""
-        jsonschema.validate(obj, schema)
+        def validate(obj, schema=schema):
+            """Validate object against schema."""
+            jsonschema.validate(obj, schema)
 
-    # Override signature
-    sig = signature(validate)
-    sig = sig.replace(parameters=tuple(sig.parameters.values())[:1])
-    validate.__signature__ = sig
+        # Override signature
+        sig = signature(validate)
+        sig = sig.replace(parameters=tuple(sig.parameters.values())[:1])
+        validate.__signature__ = sig
 
-    validate.__name__ = f'validate_{component_name}'
-    validate.__doc__ = (
-        """Validate object against {component:s} schema.
+        # Make version useable
+        _version2 = _version.split('.')
 
-        Parameters
-        ----------
-        obj : object
-            Object to validate
+        validate.__name__ = f'validate_{component_name}_{_version2[0]}_{_version2[1]}'
+        validate.__doc__ = (
+            """Validate object against {component:s} schema.
 
-        Raises
-        ------
-        `ValidationError <https://python-jsonschema.readthedocs.io/en/latest/errors/#jsonschema.exceptions.ValidationError>`_
-          If the object is not a valid {component:s}.
+            Parameters
+            ----------
+            obj : object
+                Object to validate
 
-        Examples
-        --------
-        >>> validate_{component:s}({{'message': {{}}}})
+            Raises
+            ------
+            `ValidationError <https://python-jsonschema.readthedocs.io/en/latest/errors/#jsonschema.exceptions.ValidationError>`_
+              If the object is not a valid {component:s}.
 
-        """.format(
-            component=component_name
+            Examples
+            --------
+            >>> validate_{component:s}({{'message': {{}}}})
+
+            """.format(
+                component=component_name
+            )
         )
-    )
 
-    setattr(
-        sys.modules[__name__],
-        validate.__name__,
-        validate,
-    )
+        setattr(
+            sys.modules[__name__],
+            validate.__name__,
+            validate,
+        )
 
-del validate
+    del validate
