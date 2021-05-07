eaed6717f0b5224829975098d78b95591f840767
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-07 16:19:39 -0400

    Switch to more flexible validate() API

--- reasoner_validator/util.py
+++ reasoner_validator/util.py
@@ -1,4 +1,13 @@
 """Utilities."""
+import copy
+from functools import lru_cache
+
+import requests
+import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 
 def fix_nullable(schema) -> None:
@@ -27,3 +36,21 @@ def openapi_to_jsonschema(schema) -> None:
         openapi_to_jsonschema(schema.get("items", dict()))
     if schema.pop("nullable", False):
         fix_nullable(schema)
+
+
+@lru_cache()
+def load_schema(trapi_version: str):
+    """Load schema from GitHub."""
+    response = requests.get(f"https://raw.githubusercontent.com/NCATSTranslator/ReasonerAPI/v{trapi_version}/TranslatorReasonerAPI.yaml")
+    spec = yaml.load(response.text, Loader=Loader)
+    components = spec["components"]["schemas"]
+    for component, schema in components.items():
+        openapi_to_jsonschema(schema)
+    schemas = dict()
+    for component in components:
+        # build json schema against which we validate
+        subcomponents = copy.deepcopy(components)
+        schema = subcomponents.pop(component)
+        schema["components"] = {"schemas": subcomponents}
+        schemas[component] = schema
+    return schemas
