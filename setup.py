932dca69d906dbc1dcaaf487d41dd4ba20a0a8c3
richardbruskiewich <richard.bruskiewich@delphinai.com>
2022-08-12 11:11:33 -0700

    Tagging test sub-release of 2.2.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as stream:
     long_description = stream.read()
 
 NAME = 'reasoner-validator'
-VERSION = '2.2.9.1'
+VERSION = '2.2.9.2'
 DESCRIPTION = 'Validation tools for Reasoner API'
 URL = 'https://github.com/NCATSTranslator/reasoner-validator'
 
