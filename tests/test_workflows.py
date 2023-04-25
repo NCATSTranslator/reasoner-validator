505af925a772a12af4dcbff827c777d429a9cb07
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-04-25 12:00:52 -0700

    tweak unit test to have more explicit parameters

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -65,12 +65,12 @@ SAMPLE_QUERY_2 = {
 QUERY_VERSION = [qv for qv in product(LATEST_TEST_VERSIONS, (SAMPLE_QUERY_1, SAMPLE_QUERY_2))]
 
 
-@pytest.mark.parametrize("query", QUERY_VERSION)
-def test_query_latest_trapi_workflow_properties(query: Tuple[str, Dict]):
+@pytest.mark.parametrize("trapi_version,query", QUERY_VERSION)
+def test_query_latest_trapi_workflow_properties(trapi_version: str, query: Dict):
     """Test flawed TRAPI Query workflow properties."""
 
-    print(f"\nTRAPI release: '{query[0]}'")
-    print(f"Test Workflow: '{dumps(query[1]['workflow'], indent=2)}'")
+    print(f"\nTRAPI release: '{trapi_version}'")
+    print(f"Test Workflow: '{dumps(query['workflow'], indent=2)}'")
 
-    validator = TRAPISchemaValidator(trapi_version=query[0])
-    validator.validate(query[1], "Query")
+    validator = TRAPISchemaValidator(trapi_version=trapi_version)
+    validator.validate(query, "Query")
