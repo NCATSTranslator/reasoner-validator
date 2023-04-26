3f2455eef69ecea81d9d35aa76259bcf558dc78e
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-04-26 15:23:38 -0700

    'suppress_empty_data_warnings' predicate flag added to TRAPIResponseValidator

--- reasoner_validator/__init__.py
+++ reasoner_validator/__init__.py
@@ -10,6 +10,10 @@ from reasoner_validator.biolink import (
 # in various parts TRAPI Query Response.Message
 from reasoner_validator.trapi import check_trapi_validity, TRAPISchemaValidator
 from reasoner_validator.trapi.mapping import MappingValidator, check_node_edge_mappings
+from reasoner_validator.versioning import SemVer, SemVerError
+
+import logging
+logger = logging.getLogger(__name__)
 
 # Unspoken assumption here is that validation of results returned for
 # Biolink Model release compliance only needs to be superficial
@@ -27,7 +31,8 @@ class TRAPIResponseValidator(ValidationReporter):
             trapi_version: Optional[str] = None,
             biolink_version: Optional[str] = None,
             sources: Optional[Dict] = None,
-            strict_validation: bool = False
+            strict_validation: bool = False,
+            suppress_empty_data_warnings: bool = False
     ):
         """
         :param trapi_version: version of component against which to validate the message (mandatory, no default)
@@ -35,10 +40,13 @@ class TRAPIResponseValidator(ValidationReporter):
         :param biolink_version: Biolink Model (SemVer) release against which the knowledge graph is to be
                                 validated (Default: if None, use the Biolink Model Toolkit default version).
         :type biolink_version: Optional[str] = None
-        :param sources: Dictionary of validation context identifying the ARA and KP for provenance attribute validation
+        :param sources: Dictionary of validation context identifying the ARA and KP for provenance attribute validation.
         :type sources: Dict
         :param strict_validation: if True, some tests validate as 'error'; None or False, simply issue a 'warning'
         :type strict_validation: Optional[bool] = None
+        :param suppress_empty_data_warnings: validation normally reports empty Message query graph, knowledge graph
+                       and results as warnings. This flag suppresses the reporting of such warnings (default: False).
+        :type suppress_empty_data_warnings: bool
         """
         ValidationReporter.__init__(
             self,
@@ -48,6 +56,7 @@ class TRAPIResponseValidator(ValidationReporter):
             sources=sources,
             strict_validation=strict_validation
         )
+        self.suppress_empty_data_warnings = suppress_empty_data_warnings
 
     def sanitize_trapi_query(self, response: Dict) -> Dict:
         """
@@ -70,7 +79,11 @@ class TRAPIResponseValidator(ValidationReporter):
                     step.pop('parameters')
         return response
 
-    def check_compliance_of_trapi_response(self, response: Optional[Dict], edges_limit: int = 100):
+    def check_compliance_of_trapi_response(
+            self,
+            response: Optional[Dict],
+            edges_limit: int = 100
+    ):
         """
         One stop validation of all components of a TRAPI-schema compliant
         Query.Response, including its Message against a designated Biolink Model release.
@@ -88,10 +101,10 @@ class TRAPIResponseValidator(ValidationReporter):
         :param response: Query.Response to be validated.
         :type response: Optional[Dict]
         :param edges_limit: integer maximum number of edges to be validated in the knowledge graph. A value of zero
-                            triggers validation of all edges in the knowledge graph (could take awhile! Default: 100)
+                            triggers validation of all edges in the knowledge graph (Default: 100)
         :type edges_limit: int
 
-        :returns: Validator cataloging "information", "warning" and "error" messages (may be empty)
+        :returns: Validator cataloging "information", "warning" and "error" messages (could be empty)
         :rtype: ValidationReporter
         """
         if not (response and 'message' in response):
@@ -194,12 +207,14 @@ class TRAPIResponseValidator(ValidationReporter):
         """
         # Query Graph must not be missing...
         if 'query_graph' not in message:
-            self.report(code="error.trapi.response.query_graph.missing")
+            if not self.suppress_empty_data_warnings:
+                self.report(code="error.trapi.response.query_graph.missing")
         else:
             # ... nor empty
             query_graph = message['query_graph']
             if not (query_graph and len(query_graph) > 0):
-                self.report(code="error.trapi.response.query_graph.empty")
+                if not self.suppress_empty_data_warnings:
+                    self.report(code="error.trapi.response.query_graph.empty")
             else:
                 # Validate the TRAPI compliance of the Query Graph
                 trapi_validator: TRAPISchemaValidator = check_trapi_validity(
@@ -244,7 +259,8 @@ class TRAPIResponseValidator(ValidationReporter):
 
         # The Knowledge Graph should not be missing
         if 'knowledge_graph' not in message:
-            self.report(code="error.trapi.response.knowledge_graph.missing")
+            if not self.suppress_empty_data_warnings:
+                self.report(code="error.trapi.response.knowledge_graph.missing")
         else:
             knowledge_graph = message['knowledge_graph']
             # The Knowledge Graph should also not generally be empty? Issue a warning
@@ -253,9 +269,10 @@ class TRAPIResponseValidator(ValidationReporter):
                     "nodes" in knowledge_graph and knowledge_graph["nodes"] and
                     "edges" in knowledge_graph and knowledge_graph["edges"]
             ):
-                self.report(code="warning.response.knowledge_graph.empty")
                 # An empty knowledge graph (warning) does not generally invalidate
                 # the whole Message, but no more validation tests are needed
+                if not self.suppress_empty_data_warnings:
+                    self.report(code="warning.response.knowledge_graph.empty")
             else:
                 mapping_validator: MappingValidator = check_node_edge_mappings(knowledge_graph)
                 if mapping_validator.has_messages():
@@ -304,22 +321,39 @@ class TRAPIResponseValidator(ValidationReporter):
         #     :param output_node_binding: node 'a' or 'b' of the ('one hop') QGraph test query
         #     :type output_node_binding: str
 
+        trapi_1_4_0: bool
+        try:  # try block ... Sanity check: in case the trapi_version is somehow invalid?
+            target_version: SemVer = SemVer.from_string(self.trapi_version)
+            trapi_1_4_0 = target_version >= SemVer.from_string("1.4.0")
+        except SemVerError as sve:
+            logger.error(f"has_valid_results() 'self.trapi_version' seems invalid: {str(sve)}. Reset to latest?")
+            self.trapi_version = "1.4.0"
+            trapi_1_4_0 = True
+
         # The Message.Results key should not be missing?
         if 'results' not in message:
             self.report(code="error.trapi.response.results.missing")
         else:
             results = message['results']
+
             if not (results and len(results) > 0):
-                self.report(code="warning.response.results.empty")
-                # An empty result (warning) does not generally invalidate
-                # the whole Message, but no more validation tests are needed
+                if not self.suppress_empty_data_warnings:
+                    self.report(code="warning.response.results.empty")
+                    # An empty result (warning) does not generally invalidate
+                    # the whole Message, but no more validation tests are needed
+
             elif not isinstance(results, List):
                 # The Message.results should be an array of Result objects
+                # TODO: Is this test unnecessary, since TRAPI schema
+                #       validation (below) should normally catch this?
                 self.report(code="error.trapi.response.results.non_array")
+
             else:
                 # Validate a subsample of a non-empty Message.results component.
                 results_sample = self.sample_results(results)
                 for result in results_sample:
+
+                    # generally validate against the pertinent schema
                     trapi_validator: TRAPISchemaValidator = check_trapi_validity(
                         instance=result,
                         component="Result",
@@ -329,9 +363,16 @@ class TRAPIResponseValidator(ValidationReporter):
                         # Record the error messages associated with the Result set then... don't continue
                         self.merge(trapi_validator)
 
-                    # TODO: here, we might wish to compare the Results against the contents of the KnowledgeGraph,
-                    #       with respect to node input values from the QueryGraph but this is tricky to do solely
-                    #       with the subsamples, which may not completely overlap.
+                    # Maybe some additional TRAPI-release specific non-schematic validation here?
+                    if trapi_1_4_0:
+                        pass
+                    else:
+                        pass
+
+                    # TODO: here, we could try to compare the Results against the contents of the KnowledgeGraph,
+                    #       with respect to node input values from the QueryGraph, but this is tricky to do solely
+                    #       with the subsamples, which may not completely overlap,
+                    #       and may also be somewhat computationally intensive?
 
                     # ...Finally, check that the sample Results contained the object of the Query
 
