a927e6790c66ec2d422bf1e8dc6f5a115368301f
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-05-31 14:00:21 -0700

    stub unit tests

---
+++ tests/test_versioning.py
@@ -0,0 +1,13 @@
+"""Test TRAPI version handling."""
+import pytest
+
+from reasoner_validator.versioning import get_latest_version
+
+
+def test_semver_spec_trapi_version():
+    pass
+
+
+def test_schema_spec_trapi_version():
+    pass
+
