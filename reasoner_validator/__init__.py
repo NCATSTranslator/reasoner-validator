56b9076eff39719481275d408ae867ff6e7d83f9
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-06-16 19:57:50 -0700

    comment tweak

--- reasoner_validator/__init__.py
+++ reasoner_validator/__init__.py
@@ -91,7 +91,7 @@ class TRAPIResponseValidator(ValidationReporter):
                     if 'upstream_resource_ids' not in source or source['upstream_resource_ids'] is None:
                         source['upstream_resource_ids'] = []
 
-        # 'auxiliary_graphs' (from TRAPI 1.4.0-beta2 onwards)
+        # 'auxiliary_graphs' (from TRAPI 1.4.0-beta3 onwards)
         # ought to be nullable, however... not specified that way (yet)
         if current_version >= self.TRAPI_1_4_0_BETA3 and \
                 ('auxiliary_graphs' not in response['message'] or response['message']['auxiliary_graphs'] is None):
