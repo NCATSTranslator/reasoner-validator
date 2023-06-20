b167a6fd34775428c5315732e79709a461e7a680
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-06-19 18:50:19 -0700

    Fixed test file path for unit test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,10 @@
 from sys import stderr
-from os.path import abspath, dirname
+from os.path import abspath, dirname, sep
 
 TESTS_DIRECTORY = abspath(dirname(__file__))
 print(f"Test Directory: {TESTS_DIRECTORY}", file=stderr)
 
 SAMPLE_SCHEMA_VERSION = "v3.2.1-beta5"
-SAMPLE_SCHEMA_FILE = f"{TESTS_DIRECTORY}/test_data/sample_trapi_schema_{SAMPLE_SCHEMA_VERSION}.yaml"
+SAMPLE_SCHEMA_FILE = f"{TESTS_DIRECTORY}{sep}test_data{sep}sample_trapi_schema_{SAMPLE_SCHEMA_VERSION}.yaml"
 
 LATEST_TRAPI_VERSION: str = "v1.4.0-beta4"
