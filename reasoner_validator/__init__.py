e7d765c96883a6308acfcb263168f6df2f99d86f
Richard Bruskiewich <richard@starinformatics.com>
2023-06-28 18:46:51 -0700

    ignore prerelease and builddata annotation

--- reasoner_validator/__init__.py
+++ reasoner_validator/__init__.py
@@ -534,8 +534,10 @@ class TRAPIResponseValidator(ValidationReporter):
         :return: bool, True if case S-P-O edge was found in the results
         """
         trapi_1_4_0: bool
-        try:    # try block ... Sanity check: in case the trapi_version is somehow invalid?
-            target_version: SemVer = SemVer.from_string(trapi_version)
+        try:
+            # try block ... Sanity check: in case the trapi_version is somehow invalid?
+            # ignore any prerelease/build qualifiers
+            target_version: SemVer = SemVer.from_string(trapi_version, ext_fields=[])
             trapi_1_4_0 = target_version >= SemVer.from_string("1.4.0")
         except SemVerError as sve:
             logger.warning(f"case_result_found() 'trapi_version' seems invalid: {str(sve)}. Default to latest?")
