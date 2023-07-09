67fc3a6b14bc8cad8cc911565898b304dedb605b
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-07-08 22:27:04 -0700

    Make TRAPI 1.4.2 the 'latest' release recognized by reasoner-validator

--- reasoner_validator/trapi/__init__.py
+++ reasoner_validator/trapi/__init__.py
@@ -26,12 +26,6 @@ logger = logging.getLogger(__name__)
 TRAPI_1_3_0_SEMVER = SemVer.from_string("v1.3.0")
 TRAPI_1_3_0: str = str(TRAPI_1_3_0_SEMVER)
 
-TRAPI_1_4_0_SEMVER = SemVer.from_string("v1.4.0")
-TRAPI_1_4_0: str = str(TRAPI_1_4_0_SEMVER)
-# patch version to fix 'auxiliary_graphs' model in 1.4.0
-TRAPI_1_4_1_SEMVER = SemVer.from_string("v1.4.1")
-TRAPI_1_4_1: str = str(TRAPI_1_4_1_SEMVER)
-
 TRAPI_1_4_0_BETA_SEMVER = SemVer.from_string("v1.4.0-beta")
 TRAPI_1_4_0_BETA = str(TRAPI_1_4_0_BETA_SEMVER)
 
@@ -39,7 +33,18 @@ TRAPI_1_4_0_BETA2_SEMVER = SemVer.from_string("v1.4.0-beta2")
 TRAPI_1_4_0_BETA3_SEMVER = SemVer.from_string("v1.4.0-beta3")
 TRAPI_1_4_0_BETA4_SEMVER = SemVer.from_string("v1.4.0-beta4")
 
-LATEST_TRAPI_RELEASE_SEMVER: SemVer = TRAPI_1_4_1_SEMVER
+TRAPI_1_4_0_SEMVER = SemVer.from_string("v1.4.0")
+TRAPI_1_4_0: str = str(TRAPI_1_4_0_SEMVER)
+
+# patch version to fix 'auxiliary_graphs' model in 1.4.0
+TRAPI_1_4_1_SEMVER = SemVer.from_string("v1.4.1")
+TRAPI_1_4_1: str = str(TRAPI_1_4_1_SEMVER)
+
+# patch version to fix '#components/schemas/AuxiliaryGraph' bug
+TRAPI_1_4_2_SEMVER = SemVer.from_string("v1.4.2")
+TRAPI_1_4_2: str = str(TRAPI_1_4_2_SEMVER)
+
+LATEST_TRAPI_RELEASE_SEMVER: SemVer = TRAPI_1_4_2_SEMVER
 LATEST_TRAPI_RELEASE: str = str(LATEST_TRAPI_RELEASE_SEMVER)
 
 LATEST_TRAPI_MAJOR_RELEASE_SEMVER: SemVer = SemVer.from_string("v1.4", core_fields=['major', 'minor'])
